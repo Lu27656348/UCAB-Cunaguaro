b2313d938d928669c5c94c5fc98f7b9e89f3efdb
Lucho <luiscarlossomoza@gmail.com>
2023-03-10 09:21:32 -0400

    Modelos de tablas implementados para manejo de la API
    Modelos de tablas implementados para manejo de la API

---
+++ restapi-postgres-sequelize-express-v3/src/models/Estudiantes.js
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from '../database/database.js';
+
+export const Estudiantes = sequelize.define('estudiantes', {
+    cedula: {
+        type: DataTypes.STRING(10),
+        primaryKey: true
+    },
+    nombres: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    apellidos: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true
+    },
+    telefono: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    oficina: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    habitacion: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+},{
+    timestamps:false
+});
+
+Estudiantes.hasOne(Realiza_tg, {
+    foreignKey: 'cedula_estudiante',
+    sourceKey: 'cedula'
+});
+
+Realiza_tg.belongsTo(Estudiantes, {
+    foreignKey: 'cedula_estudiante',
+    targetId: 'cedula'
+});
