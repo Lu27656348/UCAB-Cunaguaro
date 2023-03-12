3dadf9e3ae243c366daedd57ca7532f57a3c977a
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-03-11 23:34:53 -0400

    Clases para formularios
    + Empresa
    + Externo

---
+++ src/modules/formularioEmpresa.js
@@ -0,0 +1,12 @@
+export class FormularioEmpresa {
+
+  constructor(
+    nombre,
+    direccion,
+    telefono
+  ){
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+  };
+};
